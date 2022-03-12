3cc2fc8c35407c8034c7fc9b27691039be7fdec9
imran kader chowdhury <imrankaderchowdhury@imrans-MacBook-Pro.local>
2022-03-12 07:57:34 +0600

    frontend update

--- leaderboard.php
+++ leaderboard.php
@@ -1,8 +1,15 @@
 <?php
 require_once("config.php");
-    
+
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection error!");
 
 $sql = "SELECT * FROM leaderboard";
 $result = $conn->query($sql);
-echo json_encode($result);
+//var_dump($result);
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+echo json_encode($rows);
+//
+?>
